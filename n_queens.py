97a11b60a9795b4930bffff5718b0694dca43fe3
Theodoro <98555209+tgaspe@users.noreply.github.com>
2022-11-04 11:41:25 -0400

    update
    took out print statements

--- n_queens.py
+++ n_queens.py
@@ -2,12 +2,15 @@
 # McGill Id: 260842764
 #
 #  **** N-QUEENS PROBLEM ****
-#
+#  
+# How many queens on can place on a chess board without them attacking eachother?
+# On this assignment I solve this problem using two different approaches. 
+# A backtracking approach and a local search one.
 
 from random import randint
 
 def deepcopy(board):
-    '''Helper function: makes deep copy of list'''
+    '''Helper function: makes deep copy of a list'''
     copy = []
     for i in board:
         copy.append(i)
@@ -15,22 +18,20 @@ def deepcopy(board):
 
 def checkAttacks(board):
     '''
-    (list) -> int
-    Helper function: Counts number of attacks between Queens
+    (list) -> list(tuples)
+    Helper function: Counts number of attacks between Queens. 
+    Possible attacks are btw both diagonals and within the same collum (rows are excluded).
+    Returns a list of tuples, where each tuple represents an attack btw two queens. 
     '''
     n = len(board)
     attacks = 0
     conflicts = []
 
-
     i = 1
     while i < n:
-        #print(str(i) + " ------")
         j = i + 1
         while j < n:
-            #print("j " + str(j) + " => board[i] " + str(board[i]) + " board[j] " + str(board[j]))
             pair = (i, j)
-            
             if   board[i] == board[j]:         # there is a Q already at the same collum on row level i
                 conflicts.append(pair)
                 attacks += 1   
@@ -42,69 +43,66 @@ def checkAttacks(board):
                 attacks += 1      
             j += 1
         i += 1
-    #print("attacks: " + str(attacks))
-    #print(conflicts)
+
     return conflicts
 
 def createRandomBoard(n):
-    board = ["#"]   # Initializing Board
-    for i in range(1, n + 1):   # Creating broken state solution
+    '''Helper function: creates a random board.'''
+    # Initializing Board
+    board = ["#"]   
+    # Creating broken state/board solution
+    for i in range(1, n + 1):   
         board.append(randint(1, n))
     return board
     
-
+# Local Search approach: Hill Climbing
 def localSearchQueens(n):
-    '''Definition'''
+    '''
+    (int) -> list
+    This function places as many Queens(Q) as possible in a chess board of n x n size. 
+    It respect all possible Queen movements constrains not allowing to place a Queen on the same collum, row or both diagonals.
+    It uses a local search approach (hill climbing) to solve the N-Queens problem. 
+    "board" is an 1D list, where the index of the list represents the row,
+    and the list[i] the value of the collum where the queen is located.
+    "n" is the size of the board.
+    '''
 
+    # Creating initial state
     board = createRandomBoard(n)
     
-    attacks = 1         # Heuristic function initialization  
+    attacks = 1         # Heuristic function h(s)  
     while attacks != 0: # while there is attacks btw queens
 
-        neighbor = deepcopy(board)      # Initializing neighbor state board
-        pairsQueen = checkAttacks(board)# Gets list of pairs of conflicting Queens
-        attacks = len(pairsQueen)       # Get n# of attacks by queens
+        neighbor = deepcopy(board)              # Initializing neighbor state/board
+        pairsQueen = checkAttacks(board)        # Gets list of pairs of conflicting Queens
+        attacks = len(pairsQueen)               # Get n# of attacks by queens
         
-        pair = randint(0, attacks - 1)  # Selectin random conflicting pair of queens
-        queen = pairsQueen[pair][randint(0,1)]     # Selecting random Queen from the conflicting pair
+        pair = randint(0, attacks - 1)          # Selecting random conflicting pair of queens
+        queen = pairsQueen[pair][randint(0,1)]  # Selecting random Queen from the conflicting pair
         
-        #print("\nBoard " + str(board))
-        #print("p attacks: " + str(attacks))
-        #print("List conflicts " + str(pairsQueen))
-        #print("pair: " + str(pair))
-        #print("queen: " + str(queen))
+        new_col = randint(1, n)                 # Creating a random collum value
 
-        
-        new_col = randint(1, n)
-
-        if new_col != board[queen]:     # if new col value different form the current col value
-            #print("new-col: " + str(new_col))
-            neighbor[queen] = new_col   # moving conflicting queen to new collum
-            attacks1 = len(checkAttacks(neighbor)) # Calculating number of attacks for neighbor state
-            #print("attacks1 " + str(attacks1))
-            #print("neighbor: " + str(neighbor))
-
+        if new_col != board[queen]:             # if new col value != from the current col value
+            
+            # move conflicting queen to new collum
+            neighbor[queen] = new_col           # Creates a neighbor state
+            
+            # Calculating number of attacks for neighbor state
+            attacks1 = len(checkAttacks(neighbor)) 
         else: continue
         
-        
-        if attacks1 <= attacks: # if neighbor state has less or equal queens attacking eachother
-            board = neighbor
-            attacks = attacks1
+        # if neighbor state has less or equal queens attacking eachother
+        if attacks1 <= attacks: 
+            board = neighbor                    # Take Greedy Step
+            attacks = attacks1                  # Update heuristics h(s)
         else: continue
-
 
     return board[1:]
 
 
 
-#print("\nResult: " + str(localSearchQueens(8)))
 
-
-
-
-#checkAttacks(["#", 1,2,3, 4])
-
-
+# ------------------------------------------------------
 # Constructive Approach: Backtracking Search
 def distributeQueens(board, n, row = 1):
     '''
@@ -134,8 +132,7 @@ def distributeQueens(board, n, row = 1):
                 board1.insert(row, j) # Adding Queen to board
                 distributeQueens(board1, n, row + 1)
 
-
-
+# ------------------------------------------------------
 
 def printBoard(board):
     '''Helper function: prints 2d board'''
@@ -153,11 +150,17 @@ def printBoard(board):
             index += 1
         print(row)
 
+# -------- Main --------
 def main():
     '''Main'''
-    #board = [" "]       # Board is 1D list where the index represents the row and the board[i] the collum where the Queen resides 
-    n = 8               # Length of board. Eg. n = 4
+    # Length of board. Eg. n = 4
+    n = 12 
+    
+    # Backtracking approach
+    #board = [" "]             # Board is 1D list   
     #distributeQueens(board, n)
+    
+    # Local Search approach
     printBoard(localSearchQueens(n))
     
 
